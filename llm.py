3bdf7ec85d6914feac64e390168ea50b7df563b7
q-hwang <qhwang@stanford.edu>
2023-03-03 23:37:00 -0800

    add llm stuff; add history

---
+++ llm.py
@@ -0,0 +1,153 @@
+
+import openai
+import json
+
+with open("keys/key.txt", "r") as f:
+    codex_key = f.read().strip()
+openai.organization, openai.api_key = codex_key.split(":")
+
+menus = json.load(open("menu.json"))
+
+def init_llm_level_guess(user_input):
+    # e.g. I want to order some cake and I would like you to show me some of the options of the restaurant and types of the cakes
+    prompt=f"""Predict what is the user's preferred level of control over an AI recommender for each stage of ordering food from Doordash based on their input. There are four stages: selecting restaurant, selecting food, selecting delivery method, and selecting tips. Each stage has four levels of control: 0 for skip, 1 for reviewing summary, 2 for reviewing recommendation, and 3 for full user control. Note that we prefer having lower levels of controls overall to save users' time.
+
+User input: I am excited about ordering something new today
+Levels: 2 3 1 2
+
+User input: Just deliver me a burger
+Levels: 0 1 0 0
+
+User input: I want to try some tuna sashimi but I am not sure where to order from
+Levels: 2 1 2 1
+
+User input: I want some mexican food
+Levels: 1 2 0 1
+
+User input: I am hungr now, anything is fine
+Levels: 0 0 0 0
+
+User input: {user_input.strip()}
+Levels:"""
+    completion = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=10,
+        temperature=0.1,
+        n=1,
+        stop = "\n"
+    )['choices'][0]['text']
+    
+    return [int(x) for x in completion.strip().split(" ")]
+
+
+def build_prompt_history(word, local_feedback=None):
+    prompt_history = "User Input: Anything"
+    if local_feedback is not None:
+        prompt_history = ""
+        for l in local_feedback:
+            prompt_history += f"User Input: {l[0]}"
+            if len(l) > 1:
+                prompt_history += f"\nPreviously suggested {word}: {l[1]}\n\n"
+
+    prompt_history += "\nSuggested " + word + ":"
+    return prompt_history
+    
+def get_llm_restaurant_recommendation(state_dict, local_feedback=None):
+    prompt_history = "User Input: Anything"
+    if local_feedback is not None:
+        prompt_history = build_prompt_history("Restaurants", local_feedback)
+        
+    restaurants = []
+    for name, menu in menus.items():
+        restaurants.append(name)
+
+    prompt=f"""Suggest three restaurants for the user to order from based on their input, ranked from more suggested to less suggested. The restaurants are: {", ".join(restaurants)}. The sugggested three restaurants should be separated by commas.
+
+{prompt_history}"""
+    # print(prompt)
+    
+    completion = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            max_tokens=100,
+            temperature=0.1,
+            n=1,
+            stop = "\n"
+        )['choices'][0]['text']
+    
+    return completion.strip().split(",")
+
+
+
+def get_llm_food_recommendation(state_dict, local_feedback=None):
+    
+    prompt_history = "User Input: Anything"
+    if local_feedback is not None:
+        prompt_history = build_prompt_history("Dishes", local_feedback)
+
+    restaurant = state_dict[0]["selection"]
+    dishes =  list(menus[restaurant].keys())
+
+    prompt=f"""Suggest three dishes combo for the user based on their input, ranked from more suggested to less suggested. Each dish combo should inlcude 1 to three dishes, separated by commas. The dishes are: {", ".join(dishes)}. The sugggested three dishes should be separated by semicolons.
+
+{prompt_history}"""
+    # print(prompt)
+    
+    completion = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            max_tokens=100,
+            temperature=0.1,
+            n=1,
+            stop = "\n"
+        )['choices'][0]['text']
+    
+    return completion.strip().split(";")
+
+def get_llm_delivery_option_recommendation(state_dict, local_feedback=None):
+    prompt_history = "User Input: Anything"
+    if local_feedback is not None:
+        prompt_history = build_prompt_history("Method", local_feedback)
+
+    restaurant = state_dict[0]["selection"]
+
+    prompt=f"""The user has chosen to order from {restaurant}. Suggest three delivery methods from Pickup, Pickup in 15 min, Pickup in 30 min, Delivery to home, or Delivery to work for the user based on their input, ranked from more suggested to less suggested. The sugggested three methods should be separated by commas.
+
+{prompt_history}"""
+    # print(prompt)
+    
+    completion = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            max_tokens=10,
+            temperature=0.1,
+            n=1,
+            stop = "\n"
+        )['choices'][0]['text']
+    
+    return completion.strip().split(",")
+
+def get_llm_tips_option_recommendation(state_dict, local_feedback=None):
+    prompt_history = "User Input: Anything"
+    if local_feedback is not None:
+        prompt_history = build_prompt_history("Tips Amount", local_feedback)
+
+    restaurant = state_dict[0]["selection"]
+    dish = state_dict[1]["selection"]
+
+    prompt=f"""The user has chosen to order {dish} from {restaurant} via the selected delivery option \"{state_dict[2]["selection"]}\". Suggest three tips amount in us dollar for the user based on their input, ranked from more suggested to less suggested. The sugggested three tips amount should be separated by commas.
+
+{prompt_history}"""
+    print(prompt)
+    
+    completion = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            max_tokens=10,
+            temperature=0.1,
+            n=1,
+            stop = "\n"
+        )['choices'][0]['text']
+    
+    return completion.strip().split(",")
--- server.py
+++ server.py
@@ -1,8 +1,17 @@
 
 import random
+import jsonlines
+import numpy as np
+import glob
+import os
+import copy
+from llm import init_llm_level_guess, get_llm_restaurant_recommendation, get_llm_food_recommendation, get_llm_delivery_option_recommendation, get_llm_tips_option_recommendation
 debug = False
 
-STAGES = ["restaurant", "food items", "delivery_address", "tips"]
+STAGES = ["restaurant", "food items", "delivery method", "tips"]
+ADAPTATION_PENALTY = 2
+SMOOTH = 1
+CONTEXT_WEIGHT = 1
 
 def get_user_input():
     input_string = input('User:')
@@ -16,54 +25,46 @@ def get_user_input():
         message = inputs[2]
     return  False, edit_stage, button, message 
 
-def get_llm_restaurant_recommendation(state_dict, global_prompt=None, local_feedback=None):
-    if debug:
-        print("LLM on restaurant: " + global_prompt + "    " + local_feedback)
-    return random.choices(["r1", "r2", "r3"],k=2)
-
-def get_llm_food_recommendation(state_dict, global_prompt=None, local_feedback=None):
-    if debug:
-        print("LLM on food: " + global_prompt + "    " + local_feedback)
-    return random.choices(["f1", "f2", "f3"],k=2)
-
-def get_llm_delivery_option_recommendation(state_dict, global_prompt=None, local_feedback=None):
-    if debug:
-        print("LLM on delivery: " + global_prompt + "    " + local_feedback)
-    return random.choices(["d1", "d2", "d3"],k=2)
-
-def get_llm_tips_option_recommendation(state_dict, global_prompt=None, local_feedback=None):
-    if debug:
-        print("LLM on tips: " + global_prompt + "    " + local_feedback)
-    return random.choices(["t1", "t2", "t3"],k=2)
-
 init_guess_state_dict = [
     {
         "level": 1, 
-        "affects_stage": [1,3],
+        "affects_stage": [1,2,3],
+        "affected_by": [],
         "handeler": get_llm_restaurant_recommendation, 
         "selection": None,
-        "local_feedback": ""
+        "local_feedback": "",
+        "expected_adapt_time": 1,
+        "edited": False,
     },
     {
         "level": 1, 
         "affects_stage": [3],
+        "affected_by": [0],
         "handeler": get_llm_food_recommendation, 
         "selection": None,
-        "local_feedback": ""
+        "local_feedback": "",
+        "expected_adapt_time": 1,
+        "edited": False,
     },
     {
         "level": 1, 
-        "affects_stage": [],
+        "affects_stage": [3],
+        "affected_by": [0],
         "handeler": get_llm_delivery_option_recommendation, 
         "selection": None,
-        "local_feedback": ""
+        "local_feedback": "",
+        "expected_adapt_time": 1,
+        "edited": False,
     },
     {
         "level": 1, 
         "affects_stage": [],
+        "affected_by": [0, 1, 2],
         "handeler": get_llm_tips_option_recommendation, 
         "selection": None,
-        "local_feedback": ""
+        "local_feedback": "",
+        "expected_adapt_time": 1,
+        "edited": False,
     }
 ]
 
@@ -98,29 +99,102 @@ def display_full_webpage(state_dict, curr_stage_idx):
     selected_option = input('User give option:')
     return selected_option
 
+def get_init_level_guess(user_name, user_input):
+
+    # estimate this user's preference in this context    
+    context_level = np.array(init_llm_level_guess(user_input))
+
+    # estimate all user's preference
+    all_user_level_default = np.array([1,1,1,1]) # default
+    all_user_level = all_user_level_default
+
+    all_user_levels = []
+    for f in glob.glob("histories/*_levels.jsonl"):
+        user_levels = []
+        with jsonlines.open(f) as reader:
+            for obj in reader:
+                init_input, level = obj
+                user_levels.append(level)
+        all_user_levels.append(np.mean(user_levels[-SMOOTH:], axis=0)) 
+    if len(all_user_levels) != 0:
+        all_user_level = np.mean(all_user_levels, axis=0)
+
+    # estimate this user's preference
+    if os.path.exists(f"histories/{user_name}_levels.jsonl"):
+        this_user_levels = []
+        with jsonlines.open(f"histories/{user_name}_levels.jsonl") as reader:
+            for obj in reader:
+                init_input, level = obj
+                this_user_levels.append(level)
+        this_user_level = np.mean(this_user_levels[-SMOOTH:], axis=0)
+        return this_user_level * (1-CONTEXT_WEIGHT) + context_level * CONTEXT_WEIGHT
+    else:
+        return (all_user_level * len(all_user_levels) + all_user_level_default*3) / (len(all_user_levels)+3) * (1-CONTEXT_WEIGHT) + context_level* CONTEXT_WEIGHT
+
+    
+
+def get_init_adapt_guess(user_name):
+    this_user_adapt = np.array([1,1,1,1]) # default
+    if os.path.exists(f"histories/{user_name}_levels.jsonl"):
+        with jsonlines.open(f"histories/{user_name}_expected_adapt_times.jsonl") as reader:
+            for adapt in reader:
+                return adapt
+    
+    return this_user_adapt
+
+def is_finalized(state_dict, stage_idx):
+    return type(state_dict[stage_idx]["selection"]) == str
+
 
 if __name__ == "__main__":
-    print("APP: Welcome! What do you want to eat?")
+    print("APP: Welcome! What do you want?")
+    user_name = input('User Name:')
     init_input = input('User:')
     # TODO: try populate this initial guess with log.txt, heuristic + maybe with LLM using few shot prompting 
-    state_dict = init_guess_state_dict
+    init_level_guess = get_init_level_guess(user_name, init_input)
+    init_adapt_guess = get_init_adapt_guess(user_name)
+    print(init_level_guess)
+    print(init_adapt_guess)
+    for i in range(len(STAGES)):
+        init_guess_state_dict[i]["level"] = round(init_level_guess[i])
+        init_guess_state_dict[i]["expected_adapt_time"] = int(init_adapt_guess[i])
+        init_guess_state_dict[i]["local_feedback"] = [[init_input]]
+    state_dict = copy.deepcopy(init_guess_state_dict)
 
     # initial handeling
     curr_stage_idx = 0
     while True:
         if curr_stage_idx == len(STAGES):
             # user engaging screen
-            confirm, edit_stage, selected_option_idx, local_feedback, reroll, increase_level = display_summary_webpage(state_dict)
+            confirm, edit_stage, selected_option_idx, user_message, reroll, increase_level = display_summary_webpage(state_dict)
             if confirm:
                 filled = True
                 for s in range(len(STAGES)):
-                    if state_dict[s]["selection"] is None:
+                    if not is_finalized(state_dict, s):
                         print("CANNOT CONFIRM, need to complete " + STAGES[s])
                         filled = False
                         break
                 if filled:
-                    with open("log.txt", "a+") as f:
-                        f.write(str((init_input, [x["level"] for x  in state_dict])))
+                    with jsonlines.open(f"histories/{user_name}_levels.jsonl", mode='a') as writer:
+                        end_estimate = state_dict
+                        for i in range(len(state_dict)):
+                            if state_dict[i]["level"] > init_guess_state_dict[i]["level"]:
+                                if debug:
+                                    print("ADAPTATION INCREASE")
+                                end_estimate[i]["expected_adapt_time"] = end_estimate[i]["expected_adapt_time"] * ADAPTATION_PENALTY
+                            # elif not state_dict[i]["edited"]:
+                            else:
+                                if debug:
+                                    print("ADAPTATION DECREASE")
+                                end_estimate[i]["level"] = max(init_level_guess[i] - 1/state_dict[i]["expected_adapt_time"], 0)
+                
+                        writer.write((init_input, [x["level"] for x  in end_estimate]))
+
+                    with jsonlines.open(f"histories/{user_name}_expected_adapt_times.jsonl", mode='w') as writer:
+                        writer.write([x["expected_adapt_time"] for x  in end_estimate])
+
+                    # with open(f"{user_name}_log.txt", "a+") as f:
+                    #     f.write(str((init_input, [x["level"] for x  in state_dict])))
                     exit()
                 else:
                     continue
@@ -133,7 +207,8 @@ if __name__ == "__main__":
                 else:
                     curr_stage_idx = edit_stage
                     state_dict[edit_stage]["selection"] = None
-                    state_dict[curr_stage_idx]["local_feedback"] += local_feedback
+                    state_dict[edit_stage]["edited"] = True
+                    state_dict[curr_stage_idx]["local_feedback"].append([user_message])
                     if increase_level:
                         curr_level = state_dict[curr_stage_idx]["level"]
                         state_dict[curr_stage_idx]["level"] =  min(curr_level + 1, 3)
@@ -145,7 +220,7 @@ if __name__ == "__main__":
         if debug:
             print("handling: " + stage_name, stage_level)
             print(state_dict)
-        if state_dict[curr_stage_idx]["selection"] is not None:
+        if state_dict[curr_stage_idx]["selection"] is not None or any([not is_finalized(state_dict, s) for s in  state_dict[curr_stage_idx]["affected_by"]]):
             curr_stage_idx += 1 
             continue
 
@@ -157,8 +232,9 @@ if __name__ == "__main__":
             # recommend to user
 
             local_feedback = state_dict[curr_stage_idx]["local_feedback"]
-            llm_suggestions = state_dict[curr_stage_idx]["handeler"](state_dict, init_input, local_feedback)
+            llm_suggestions = state_dict[curr_stage_idx]["handeler"](state_dict, local_feedback)
             state_dict[curr_stage_idx]["selection"] = llm_suggestions
+            init_guess_state_dict[i]["local_feedback"][-1].append(llm_suggestions)
             curr_stage_idx = len(STAGES)
             continue
 
@@ -166,8 +242,9 @@ if __name__ == "__main__":
             # skip
 
             local_feedback = state_dict[curr_stage_idx]["local_feedback"]
-            llm_suggestions = state_dict[curr_stage_idx]["handeler"](state_dict, init_input, local_feedback)
+            llm_suggestions = state_dict[curr_stage_idx]["handeler"](state_dict, local_feedback)
             state_dict[curr_stage_idx]["selection"] = llm_suggestions[0]
+            init_guess_state_dict[i]["local_feedback"][-1].append(llm_suggestions[0])
         
         for s in state_dict[curr_stage_idx]["affects_stage"]:
             state_dict[s]["selection"] = None
